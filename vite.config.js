7249803676a44badd3aeba6af988d2a9c3e5bf86
HolaStranger <vikneswari4527773@gmail.com>
2026-05-05 21:42:51 +0800

    final

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',  // ← add this line
+  base: '/',  // ← add this line
 
   plugins: [react()],
   resolve: {
